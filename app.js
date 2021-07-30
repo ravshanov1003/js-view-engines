62c68b45177b7dd3014b9b2bda660f6af640fe3c
ravshanov1003 <ravshanov1003@gmail.com>
2021-07-30 16:25:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+
+const app = express()
+
+// register view engine
+app.set('view engine', 'ejs');
+
+
+app.listen(3000, () => console.log("Server is running"))
+
+app.get("/", (req, res) => {
+    const blogs = [
+        { title: 'First blog', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+        { title: 'Second blog', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+        { title: 'Third blog', snippet: 'Lorem ipsum dolor sit amet consectetur.' }
+    ]
+    res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+})
+
+app.get('/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' })
+})
+
+app.get('/about-me', (req, res) => {
+    res.redirect('/about')
+})
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - OOPS !</h1>')
+})
