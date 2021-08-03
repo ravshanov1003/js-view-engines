5bbf44f47124b194ae97d88df1b2e50c61f1b629
ravshanov1003 <ravshanov1003@gmail.com>
2021-08-03 11:51:53 +0500

    added blogRoutes

--- app.js
+++ app.js
@@ -55,36 +55,7 @@ app.get('/about', (req, res) => {
 })
 
 // blog route
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => res.render('index', { title: "all blogs", blogs: result }))
-        .catch(err => console.log(err))
-})
 
-app.post("/blogs", (req, res) => {
-    const blog = new Blog(req.body)
-
-    blog.save()
-        .then((result) => res.redirect('/blogs', ))
-        .catch(err => console.log(err))
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => res.render('details', { blog: result, title: "Blog details" }))
-        .catch(err => console.log(err))
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => res.json({ redirect: '/blogs' }))
-        .catch(err => console.log(err))
-})
-app.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' })
-})
 app.get('/about-me', (req, res) => {
     res.redirect('/about')
 })
